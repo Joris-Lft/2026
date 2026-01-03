48902ad3abf18030544bf4d04f5b2e72982577c1
Marie-Garde <marie_garde@ymail.com>
2026-01-03 11:50:42 +0100

    add tracking daily with fake data

---
+++ 2026-app/components/H2.tsx
@@ -0,0 +1,31 @@
+import { ThemedText } from "@/components/themed-text";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { StyleSheet, TextStyle } from "react-native";
+
+type H2Props = {
+  children: React.ReactNode;
+  color?: string;
+  style?: TextStyle;
+};
+
+export default function H2({ children, color, style }: H2Props) {
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? "light"];
+
+  return (
+    <ThemedText
+      type="subtitle"
+      style={[styles.h2, { color: color ?? colors.text }, style]}
+    >
+      {children}
+    </ThemedText>
+  );
+}
+
+const styles = StyleSheet.create({
+  h2: {
+    fontSize: 24,
+    fontWeight: "600",
+  },
+});
